401f2e04e6ed1955275d70c27b3e5e3ec990eb62
zMOHAXz <vlastorz@gmail.com>
2024-06-19 19:22:58 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string=input("День или вечер?: ")
+print("Прекрасный летний",my_string)
+my_string="Прекрасный ЛЕТНИЙ вечер"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
